8d0acee9342e3f722094170f660d0e46a553bcfb
Admin <Admin@DESKTOP-146IHI4>
2023-09-21 14:30:12 +0530

    venki commit

--- src/com/lbrce/cseAsec/Sample.java
+++ src/com/lbrce/cseAsec/Sample.java
@@ -5,10 +5,15 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		vamsi();
+		venki();
 	}
 	public static void vamsi() {
 		// TODO Auto-generated method stub
 		System.out.println("This is team lead : vamsi");
+	}
+	public static void venki() {
+		// TODO Auto-generated method stub
+		System.out.println("This is team member : venki");
 	}
 
 }
