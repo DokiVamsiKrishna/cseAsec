a70aea286d3fb54b0f7ac789f2fdc9a36f1f9fa1
AdmiN <AdmiN@ITWS10>
2023-09-21 14:30:27 +0530

    Harsha commit

--- src/com/lbrce/cseAsec/Sample.java
+++ src/com/lbrce/cseAsec/Sample.java
@@ -10,5 +10,9 @@ public class Sample {
 		// TODO Auto-generated method stub
 		System.out.println("This is team lead : vamsi");
 	}
+	public static void harsha() {
+		// TODO Auto-generated method stub
+		System.out.println("This is team member : harsha");
+	}
 
 }
