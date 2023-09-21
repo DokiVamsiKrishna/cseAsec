db4d54f505a9a041ee417a3b57bd19cbd9744d37
Admin <Admin@DESKTOP-5BD9RGD>
2023-09-21 14:02:15 +0530

    Team lead commit

---
+++ src/com/lbrce/cseAsec/Sample.java
@@ -0,0 +1,14 @@
+package com.lbrce.cseAsec;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		vamsi();
+	}
+	public static void vamsi() {
+		// TODO Auto-generated method stub
+		System.out.println("This is team lead : vamsi");
+	}
+
+}
